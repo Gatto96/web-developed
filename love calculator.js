aeb7a0c0042e94b681f165c2434af9f8284a3bfd
Fernando Ivan Salas <fer.ivan.gatto96@gmail.com>
2023-03-22 20:45:55 -0600

    Love Calculator 1.0
    A simple code to "calculate" how much you match whit a person.  the number that shows you its random.

---
+++ love calculator.js
@@ -0,0 +1,14 @@
+alert("Welcome, to love calculator!")
+prompt("Introduse the one name: ")
+prompt("Introduse the second name: ")
+
+
+    var n= Math.random();
+    n= n*101;
+    n= Math.floor(n);
+    
+if (n>75) { alert ("Your Mather it's so much compatible, " + n + "%")
+    
+} else {
+    alert ("Your Mather is: " + n + "%")
+}
